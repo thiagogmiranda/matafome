f1febef05bb3e1d8c264ecde96dab22e0cc7e8ea
thiagogmiranda <thiagogmiranda.br@gmail.com>
2015-09-15 14:51:43 -0300

    TaskAsync para obter barracas dentro da localização do usuário

---
+++ app/src/main/java/br/com/unigranrio/matafome/aplicacao/client/ObterBarracasDentroDoRaioAsyncTask.java
@@ -0,0 +1,30 @@
+package br.com.unigranrio.matafome.aplicacao.client;
+
+import android.os.AsyncTask;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.unigranrio.matafome.dominio.modelo.Negocio;
+/**
+ * Created by WebFis33 on 15/09/2015.
+ */
+public class ObterBarracasDentroDoRaioAsyncTask extends AsyncTask<Double, Void, List<Negocio>> {
+    @Override
+    protected List<Negocio> doInBackground(Double... params) {
+        double raio = params[0];
+        double lat = params[1];
+        double lng = params[2];
+
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+        List<Negocio> retorno = restTemplate.getForObject("https://matafomeserver.herokuapp.com/negocio/obter-todos-dentro-raio?raio=" + raio
+                + "&lat=" + lat + "&lng=" + lng, ArrayList.class);
+
+        return retorno;
+    }
+}
