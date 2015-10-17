9f07beeecea2d0c631e263496ff909656b975bcc
thiagogmiranda <thiagogmiranda.br@gmail.com>
2015-10-16 21:28:56 -0300

    Parte da tela de cadastro de ponto de venda

--- app/src/main/java/br/com/unigranrio/matafome/aplicacao/ListaPontosVendaActivity.java
+++ app/src/main/java/br/com/unigranrio/matafome/aplicacao/ListaPontosVendaActivity.java
@@ -1,9 +1,12 @@
 package br.com.unigranrio.matafome.aplicacao;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -46,6 +49,26 @@ public class ListaPontosVendaActivity extends AppCompatActivity implements OnAsy
         obterBarracasUsuarioAsyncTask.setProgressDialog(progressDialog);
 
         atualizarLista();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_lista_ponto_venda, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.cadastrar_ponto_venda) {
+            Intent intent = new Intent();
+            intent.setClass(this, CadastrarPontoVendaActivity.class);
+
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void atualizarLista(){
---
+++ app/src/main/java/br/com/unigranrio/matafome/aplicacao/webservices/CadastrarPontoVendaAsyncService.java
@@ -0,0 +1,7 @@
+package br.com.unigranrio.matafome.aplicacao.webservices;
+
+/**
+ * Created by WebFis33 on 16/10/2015.
+ */
+public class CadastrarPontoVendaAsyncService {
+}
