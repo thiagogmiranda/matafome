7985625b8071ce42573b972221be791f77f11b50
thiagogmiranda <thiagogmiranda.br@gmail.com>
2015-11-10 23:47:48 -0200

    Adaptação do serviço de carregamento de dados do negócio do usuário para retornar um ResultadoAcao, permitindo a transmissão de mensagens em caso de erros

--- app/src/main/java/br/com/unigranrio/matafome/aplicacao/webservices/ObterNegocioUsuarioAsyncService.java
+++ app/src/main/java/br/com/unigranrio/matafome/aplicacao/webservices/ObterNegocioUsuarioAsyncService.java
@@ -3,19 +3,24 @@ package br.com.unigranrio.matafome.aplicacao.webservices;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import br.com.unigranrio.matafome.R;
 import br.com.unigranrio.matafome.aplicacao.App;
+import br.com.unigranrio.matafome.dominio.acoes.Mensagem;
+import br.com.unigranrio.matafome.dominio.acoes.ResultadoAcao;
 import br.com.unigranrio.matafome.dominio.modelo.Negocio;
 
 /**
  * Created by WebFis33 on 15/10/2015.
  */
-public class ObterNegocioUsuarioAsyncService extends AsyncTaskAbstrata<String, Void, List<Negocio>> {
+public class ObterNegocioUsuarioAsyncService extends AsyncTaskAbstrata<String, Void, ResultadoAcao<Negocio>> {
     @Override
-    protected List<Negocio> doInBackground(String... params) {
+    protected ResultadoAcao<Negocio> doInBackground(String... params) {
+        ResultadoAcao<Negocio> resultado = new ResultadoAcao<>();
+
         String email = params[0];
 
         RestTemplate restTemplate = new RestTemplate();
@@ -23,14 +28,16 @@ public class ObterNegocioUsuarioAsyncService extends AsyncTaskAbstrata<String, V
 
         String url = App.montarUrlRest(R.string.url_obter_pontos_venda_usuario, email);
 
-        Negocio[] retorno = new Negocio[0];
-
         try {
-            retorno = restTemplate.getForObject(url, Negocio[].class);
+            Negocio negocio = restTemplate.getForObject(url, Negocio.class);
+            resultado.setData(negocio);
         } catch (Exception e){
-            e.printStackTrace();
+            List<Mensagem> erros = new ArrayList<>();
+            erros.add(new Mensagem("Erro ao carregar dados, verifique sua conexão com a internet."));
+
+            resultado.adicionarMensagens(erros);
         }
 
-        return Arrays.asList(retorno);
+        return resultado;
     }
 }
