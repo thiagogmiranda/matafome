c9f4da09a4dc15a72d90a5255a1f028627b7581d
thiagogmiranda <thiagogmiranda.br@gmail.com>
2015-11-17 17:10:37 -0200

    Criação da casca para a tela de envio de avaliações

---
+++ app/src/main/java/br/com/unigranrio/matafome/aplicacao/EnviarAvaliacaoActivity.java
@@ -0,0 +1,15 @@
+package br.com.unigranrio.matafome.aplicacao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.unigranrio.matafome.R;
+
+public class EnviarAvaliacaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enviar_avaliacao);
+    }
+}
